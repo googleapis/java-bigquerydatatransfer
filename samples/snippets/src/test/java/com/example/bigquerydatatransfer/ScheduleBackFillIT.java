00d57526ef1b9b3bd9f462b8289b17158a1a0aba
Praful Makani <praful@qlogic.io>
2020-10-14 16:07:52 +0530

    docs(samples): remove service account code

--- samples/snippets/src/test/java/com/example/bigquerydatatransfer/ScheduleBackFillIT.java
+++ samples/snippets/src/test/java/com/example/bigquerydatatransfer/ScheduleBackFillIT.java
@@ -19,7 +19,6 @@ package com.example.bigquerydatatransfer;
 import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.TestCase.assertNotNull;
 
-import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.bigquery.DatasetInfo;
@@ -90,8 +89,6 @@ public class ScheduleBackFillIT {
     bigquery = BigQueryOptions.getDefaultInstance().getService();
     bigquery.create(DatasetInfo.of(datasetName));
 
-    ServiceAccountCredentials credentials =
-        (ServiceAccountCredentials) ServiceAccountCredentials.getApplicationDefault();
     // create a scheduled query
     String query =
         "SELECT CURRENT_TIMESTAMP() as current_time, @run_time as intended_run_time, "
@@ -118,11 +115,9 @@ public class ScheduleBackFillIT {
           CreateTransferConfigRequest.newBuilder()
               .setParent(parent.toString())
               .setTransferConfig(transferConfig)
-              .setServiceAccountName(credentials.getClientEmail())
               .build();
       name = client.createTransferConfig(request).getName();
       System.out.println("Scheduled query created successfully :" + name);
-      System.out.println("Service account : " + credentials.getClientEmail());
     }
   }
 

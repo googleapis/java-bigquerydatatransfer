85825cfa0bd5001e5bd4dd1a55067b81d207446f
yoshi-automation <yoshi-automation@google.com>
2020-12-18 14:39:49 -0800

    chore: update gapic-generator-java to 0.0.9
    Committer: @miraleung
    PiperOrigin-RevId: 346372957
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Dec 8 11:37:30 2020 -0800
    Source-Repo: googleapis/googleapis
    Source-Sha: 857f70daef9f4c38f042fb3dfb7b8423ae18fd19
    Source-Link: https://github.com/googleapis/googleapis/commit/857f70daef9f4c38f042fb3dfb7b8423ae18fd19

--- google-cloud-bigquerydatatransfer/src/main/java/com/google/cloud/bigquery/datatransfer/v1/stub/GrpcDataTransferServiceStub.java
+++ google-cloud-bigquerydatatransfer/src/main/java/com/google/cloud/bigquery/datatransfer/v1/stub/GrpcDataTransferServiceStub.java
@@ -286,11 +286,21 @@ public class GrpcDataTransferServiceStub extends DataTransferServiceStub {
         DataTransferServiceStubSettings.newBuilder().build(), clientContext, callableFactory);
   }
 
+  /**
+   * Constructs an instance of GrpcDataTransferServiceStub, using the given settings. This is
+   * protected so that it is easy to make a subclass, but otherwise, the static factory methods
+   * should be preferred.
+   */
   protected GrpcDataTransferServiceStub(
       DataTransferServiceStubSettings settings, ClientContext clientContext) throws IOException {
     this(settings, clientContext, new GrpcDataTransferServiceCallableFactory());
   }
 
+  /**
+   * Constructs an instance of GrpcDataTransferServiceStub, using the given settings. This is
+   * protected so that it is easy to make a subclass, but otherwise, the static factory methods
+   * should be preferred.
+   */
   protected GrpcDataTransferServiceStub(
       DataTransferServiceStubSettings settings,
       ClientContext clientContext,
